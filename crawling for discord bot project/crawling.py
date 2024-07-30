e93dcd8ae302fe2ca498feac68d8aa019099cd9d
sung0911 <tjdwnsgur0911@gmail.com>
2024-07-31 02:21:33 +0900

    rename; notice -> post, and fix errors

--- crawling for discord bot project/crawling.py
+++ crawling for discord bot project/crawling.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from info import url
+from info import urls
 
 
 def get_html(url):
@@ -12,8 +12,8 @@ def get_html(url):
 
 
 def parse_html(html):
-    soup = BeautifulSoup(get_html(url["board_url"]), 'html.parser')
-    notices = []
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = []
     rows = soup.find_all("tr")
     for row in rows:
         number = row.find('td', class_="b-num-box")
@@ -21,29 +21,29 @@ def parse_html(html):
         date = row.find('span', class_="b-date")
         writer = row.find('span', class_="b-writer")
         views = row.find('span', class_="hit")
-        notice_data = {'번호': number.get_text(strip=True) if number else '',
+        post_data = {'번호': number.get_text(strip=True) if number else '',
                        '제목': title_box.a.get_text(strip=True) if title_box and title_box.a else '',
                        "작성자": writer.get_text(strip=True) if writer else '',
                        '날짜': date.get_text(strip=True) if writer else '',
                        "조회수": views.get_text(strip=True) if views else '',
-                       "본문링크": url["board_url"] + title_box.a['href'] if title_box and title_box.a else ''}
-        notices.append(notice_data)
-    return notices
+                       "본문링크": url + title_box.a['href'] if title_box and title_box.a else ''}
+        posts.append(post_data)
+    return posts
 
 
-def clean_data(notices):
-    notices.pop(0)
-    for notice in notices:
-        view = notice["조회수"].replace("조회수", "").strip()
-        notice["조회수"] = view
-    return notices
+def clean_data(posts):
+    posts.pop(0)
+    for post in posts:
+        view = post["조회수"].replace("조회수", "").strip()
+        post["조회수"] = view
+    return posts
 
 
 if __name__ == "__main__":
-    url = url["board_url"]
+    url = urls["board_url"]
     html = get_html(url)
-    notices = parse_html(html)
-    clean_data(notices)
+    posts = parse_html(html)
+    print(clean_data(posts))
 
 
 
--- crawling for discord bot project/info.py
+++ crawling for discord bot project/info.py
@@ -1,1 +1,1 @@
-url = {"board_url": "https://ai.cnu.ac.kr/ai/board/notice.do?"}
+urls = {"board_url": "https://ai.cnu.ac.kr/ai/board/notice.do?"}
