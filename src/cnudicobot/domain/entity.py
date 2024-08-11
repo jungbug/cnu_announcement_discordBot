dfd202cbb53e0b83ecf9f96cece2115001a9a22b
BREW <IRACK000@gmail.com>
2024-08-11 19:47:23 +0900

    Feat: Apply Singleton

--- src/cnudicobot/domain/entity.py
+++ src/cnudicobot/domain/entity.py
@@ -4,6 +4,13 @@
 
 
 class NoticeDiscordBot:
+    _object = None  # singleton instance
+
+    def __new__(cls, *args, **kwargs):
+        if cls._object is None:
+            cls._object = super(NoticeDiscordBot, cls).__new__(cls)
+        return cls._object
+
     def __init__(self):
         """ Register use cases """
         pass
