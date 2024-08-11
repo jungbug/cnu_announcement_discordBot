a6da9ee5173b0ead53f149088f08c2a0268de10a
BREW <IRACK000@gmail.com>
2024-08-11 17:08:10 +0900

    Refactor: Re-construct project structures

--- src/cnudicobot/driver/env/settings.py
+++ src/cnudicobot/driver/env/settings.py
@@ -8,3 +8,31 @@ discord_token = os.getenv("DISCORD_TOKEN")
 discord_channel_id = os.getenv("DISCORD_CHANNEL_ID")
 cnu_base_url = os.getenv("BASE_URL")
 cnu_front_url = os.getenv("FRONT_URL")
+
+
+class TargetBoardURLS(dict):
+    BASE_URL = "https://ai.cnu.ac.kr/ai/board/"
+
+    @classmethod
+    def get_url(cls, board_id: str) -> str:
+        return cls.BASE_URL + board_id + ".do"
+
+    def __init__(self):
+        self.notice = self.get_url("notice")            # 학사공지
+        self.news = self.get_url("news")                # 교내 일반소식
+        self.job = self.get_url("job")                  # 교외 활동.인턴.취업
+        self.unit_news = self.get_url("unit-news")      # 사업단소식
+        self.depart_news = self.get_url("depart-news")  # 학과 News
+        self.events = self.get_url("events")            # 학과주최행사
+
+        super().__init__(
+            학사공지=self.notice,
+            교내일반소식=self.news,
+            교외활동인턴취업=self.job,
+            사업단소식=self.unit_news,
+            학과News=self.depart_news,
+            학과주최행사=self.events
+        )
+
+
+target_board_urls = TargetBoardURLS()
