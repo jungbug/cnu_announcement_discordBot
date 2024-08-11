2acfe5f10e8e3c1dee79d4f9cc8280ed54bf09c4
BREW <IRACK000@gmail.com>
2024-08-11 18:57:12 +0900

    Chore: Temporary .env configuration

--- src/cnudicobot/driver/env/settings.py
+++ src/cnudicobot/driver/env/settings.py
@@ -1,13 +1,14 @@
-from dotenv import load_dotenv
+import dotenv
 import os
 
+# Load environment variables
+dotenv.load_dotenv()
 
-load_dotenv()
 
-discord_token = os.getenv("DISCORD_TOKEN")
-discord_channel_id = os.getenv("DISCORD_CHANNEL_ID")
-cnu_base_url = os.getenv("BASE_URL")
-cnu_front_url = os.getenv("FRONT_URL")
+# Discord API Settings
+class DiscordAPISettings:
+    discord_token = os.getenv("DISCORD_TOKEN")
+    discord_channel_id = os.getenv("DISCORD_CHANNEL_ID")
 
 
 # Database settings
